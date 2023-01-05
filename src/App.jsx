1422df9e70e8667f06a6abffa63b5be18ef18d94
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2023-01-05 15:23:54 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ const [lat,lon]=searchData.value.split("");
 const currentWeather=fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
 const forecastFetch=fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
 
+
+
 Promise.all([currentWeather, forecastFetch])
 .then(async (response)=>{
   const weatherResponse=await response[0].json();
